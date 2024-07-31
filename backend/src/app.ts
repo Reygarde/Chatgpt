b53b94dfeaa7f9ce46b184aa93275890ce54a570
kevin <kevin.schoubert@accenture.com>
2024-07-31 13:44:26 +0200

    v8

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,40 +4,17 @@ import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 config();
 const app = express();
 
-// Define allowed origins
-const allowedOrigins = [
-  "http://localhost:5173", // Local development
-  "https://chatgpt-opal-mu.vercel.app", // Vercel deployment
-];
-
-// CORS middleware
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        return callback(
-          new Error("The CORS policy does not allow access from this origin."),
-          false
-        );
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  })
-);
-
+//middlewares
+app.use(cors({ origin: "https://chatgpt-opal-mu.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// Remove in production
+//remove it in production
 app.use(morgan("dev"));
 
-// Routes
 app.use("/api/v1", appRouter);
 
 export default app;
